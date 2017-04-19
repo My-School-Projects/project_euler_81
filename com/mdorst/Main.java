51f4a72c0a105df7aa51ce843b3430deb7c307bd
Michael Dorst <code@mikefdorst.com>
2017-04-19 01:30:08 -0700

    WIP

--- com/mdorst/Main.java
+++ com/mdorst/Main.java
@@ -1,8 +1,46 @@
 package com.mdorst;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
 
+    }
+
+    public static Node createTree(String path)
+    {
+        try (Scanner scanner = new Scanner(new File(path)))
+        {
+            Node root = new Node(scanner.nextInt());
+            Node row_start = root;
+            Node node_above = null;
+            for (int i = 0; i < 80; i++)
+            {
+                Node n = row_start;
+                for (int j = 1; j < 80; j++)
+                {
+                    n.right = new Node(scanner.nextInt());
+                    // for all rows except the top row...
+                    if (i > 0)
+                    {
+                        node_above.down = n;
+                        node_above = node_above.right;
+                    }
+                    n = n.right;
+                }
+                node_above = row_start;
+                row_start.down = new Node(scanner.nextInt());
+                row_start = row_start.down;
+            }
+            return root;
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+            return new Node(0);
+        }
     }
 }
---
+++ com/mdorst/Node.java
@@ -0,0 +1,10 @@
+package com.mdorst;
+
+class Node {
+    int value;
+    Node right, down;
+    Node(int value)
+    {
+        this.value = value;
+    }
+}
